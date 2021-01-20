f3d8034a0e9d246c6168b477beac22bf1a4050bc
taypark <interrupt@kakao.com>
2021-01-20 20:14:02 +0900

    Add LCM and GCD

--- cheat_sheet.py
+++ cheat_sheet.py
@@ -60,3 +60,17 @@ print(a, b, c)
 # 101101 -> [1, 0, 1, 1, 0, 1]
 each_num = list(map(int, input()))
 print(each_num)
+
+###
+# 최소공배수, 최대공약수(LCM, GCD)
+###
+
+def gcd(a, b):
+    while b != 0:
+        r = a % b
+        a = b
+        b = r
+    return a
+
+def lcm(a, b):
+    return a * b / gcd(a, b)
